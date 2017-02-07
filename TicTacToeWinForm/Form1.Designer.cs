34f63f7955b0415a3f27238d14782be2c7109e50
Thierry Judge <tejudge@gmail.com>
2017-02-07 17:10:59 -0500

    first Commit

---
+++ TicTacToeWinForm/Form1.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace TicTacToeWinForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.x3 = new System.Windows.Forms.Button();
+            this.x2 = new System.Windows.Forms.Button();
+            this.x1 = new System.Windows.Forms.Button();
+            this.y3 = new System.Windows.Forms.Button();
+            this.y2 = new System.Windows.Forms.Button();
+            this.y1 = new System.Windows.Forms.Button();
+            this.z3 = new System.Windows.Forms.Button();
+            this.z2 = new System.Windows.Forms.Button();
+            this.z1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // x3
+            // 
+            this.x3.Location = new System.Drawing.Point(410, 27);
+            this.x3.Name = "x3";
+            this.x3.Size = new System.Drawing.Size(150, 150);
+            this.x3.TabIndex = 5;
+            this.x3.UseVisualStyleBackColor = true;
+            this.x3.Click += new System.EventHandler(this.buttonClick);
+            // 
+            // x2
+            // 
+            this.x2.Location = new System.Drawing.Point(243, 27);
+            this.x2.Name = "x2";
+            this.x2.Size = new System.Drawing.Size(150, 150);
+            this.x2.TabIndex = 4;
+            this.x2.UseVisualStyleBackColor = true;
+            this.x2.Click += new System.EventHandler(this.buttonClick);
+            // 
+            // x1
+            // 
+            this.x1.Location = new System.Drawing.Point(75, 27);
+            this.x1.Name = "x1";
+            this.x1.Size = new System.Drawing.Size(150, 150);
+            this.x1.TabIndex = 3;
+            this.x1.UseVisualStyleBackColor = true;
+            this.x1.Click += new System.EventHandler(this.buttonClick);
+            // 
+            // y3
+            // 
+            this.y3.Location = new System.Drawing.Point(410, 183);
+            this.y3.Name = "y3";
+            this.y3.Size = new System.Drawing.Size(150, 150);
+            this.y3.TabIndex = 8;
+            this.y3.UseVisualStyleBackColor = true;
+            this.y3.Click += new System.EventHandler(this.buttonClick);
+            // 
+            // y2
+            // 
+            this.y2.Location = new System.Drawing.Point(243, 183);
+            this.y2.Name = "y2";
+            this.y2.Size = new System.Drawing.Size(150, 150);
+            this.y2.TabIndex = 7;
+            this.y2.UseVisualStyleBackColor = true;
+            this.y2.Click += new System.EventHandler(this.buttonClick);
+            // 
+            // y1
+            // 
+            this.y1.Location = new System.Drawing.Point(75, 183);
+            this.y1.Name = "y1";
+            this.y1.Size = new System.Drawing.Size(150, 150);
+            this.y1.TabIndex = 6;
+            this.y1.UseVisualStyleBackColor = true;
+            this.y1.Click += new System.EventHandler(this.buttonClick);
+            // 
+            // z3
+            // 
+            this.z3.Location = new System.Drawing.Point(410, 354);
+            this.z3.Name = "z3";
+            this.z3.Size = new System.Drawing.Size(150, 150);
+            this.z3.TabIndex = 11;
+            this.z3.UseVisualStyleBackColor = true;
+            this.z3.Click += new System.EventHandler(this.buttonClick);
+            // 
+            // z2
+            // 
+            this.z2.Location = new System.Drawing.Point(243, 354);
+            this.z2.Name = "z2";
+            this.z2.Size = new System.Drawing.Size(150, 150);
+            this.z2.TabIndex = 10;
+            this.z2.UseVisualStyleBackColor = true;
+            this.z2.Click += new System.EventHandler(this.buttonClick);
+            // 
+            // z1
+            // 
+            this.z1.Location = new System.Drawing.Point(75, 354);
+            this.z1.Name = "z1";
+            this.z1.Size = new System.Drawing.Size(150, 150);
+            this.z1.TabIndex = 9;
+            this.z1.UseVisualStyleBackColor = true;
+            this.z1.Click += new System.EventHandler(this.buttonClick);
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(711, 532);
+            this.Controls.Add(this.z3);
+            this.Controls.Add(this.z2);
+            this.Controls.Add(this.z1);
+            this.Controls.Add(this.y3);
+            this.Controls.Add(this.y2);
+            this.Controls.Add(this.y1);
+            this.Controls.Add(this.x3);
+            this.Controls.Add(this.x2);
+            this.Controls.Add(this.x1);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button A1;
+        private System.Windows.Forms.Button A2;
+        private System.Windows.Forms.Button A3;
+        private System.Windows.Forms.Button B3;
+        private System.Windows.Forms.Button B2;
+        private System.Windows.Forms.Button B1;
+        private System.Windows.Forms.Button C4;
+        private System.Windows.Forms.Button C2;
+        private System.Windows.Forms.Button C1;
+        private System.Windows.Forms.Button x3;
+        private System.Windows.Forms.Button x2;
+        private System.Windows.Forms.Button x1;
+        private System.Windows.Forms.Button y3;
+        private System.Windows.Forms.Button y2;
+        private System.Windows.Forms.Button y1;
+        private System.Windows.Forms.Button z3;
+        private System.Windows.Forms.Button z2;
+        private System.Windows.Forms.Button z1;
+    }
+}
+
---
+++ TicTacToeWinForm/Form1.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TicTacToeWinForm
+{
+    public partial class Form1 : Form
+    {
+
+        bool xTurn = true;
+        int numTurn = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonClick(object sender, EventArgs e)
+        {
+            Button b = (Button)sender;
+            if (xTurn)
+            {
+                b.Text = "X";
+                xTurn = !xTurn;
+                b.Enabled = false;
+                numTurn++;
+            }
+            else
+            {
+                b.Text = "O";
+                xTurn = !xTurn;
+                b.Enabled = false;
+                numTurn++;
+           
+            }
+            if (checkWin())
+            {
+                if (xTurn)
+                {
+                    MessageBox.Show("O WIN");
+                }
+                else
+                {
+                    MessageBox.Show("X WIN");
+                }
+                if (numTurn > 7)
+                {
+                    MessageBox.Show("DRAW");
+                }
+            }
+        }
+
+        private bool checkWin()
+        {
+            //check rows
+            if (x1.Text == x2.Text && x2.Text == x3.Text && x1.Text != "")
+            {
+                return true;
+            }
+            else if (y1.Text == y2.Text && y2.Text == y3.Text && y1.Text != "")
+            {
+                return true;
+            }
+            else  if (z1.Text == z2.Text && z2.Text == z3.Text && z1.Text != "")
+            {
+                return true;
+            }
+
+            //check colums;
+            else if (x1.Text == y1.Text && y1.Text == z1.Text && x1.Text != "")
+            {
+                return true;
+            }
+            else if (x2.Text == y2.Text && y2.Text == z2.Text && x2.Text != "")
+            {
+                return true;
+            }
+            else if (x3.Text == y3.Text && y3.Text == z3.Text && x3.Text != "")
+            {
+                return true;
+            }
+
+            // check diagonals
+            else if (x1.Text == y2.Text && y2.Text == z3.Text && y2.Text != "")
+            {
+                return true;
+            }
+            else if (x3.Text == y2.Text && y2.Text == z1.Text && y2.Text != "")
+            {
+                return true;
+            }
+
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
